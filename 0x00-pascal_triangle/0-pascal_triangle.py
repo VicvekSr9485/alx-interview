9e81d2a4fb0398f062d98754b78b3f71299d3e08
VicvekSr <Olamideoso65@gmail.com>
2022-12-22 23:53:09 +0000

    add pascal triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,17 +1,32 @@
 #!/usr/bin/python3
-""" pascal triangle 
-"""
-
-from math import factorial
+"""Pascal's Triangle"""
 
 
 def pascal_triangle(n):
-    """ returns pascal triangle
     """
+    a function that returns a list
+    of integers representing the
+    pascal triangle of n:
+       . Returns an empty list if n <= 0
+       . assume n will be always an integer
+    """
+    pascal_tri = []
+
     if n <= 0:
         return []
-    else:
-        for i in range(n):
-            for j in range(0, i + 1):
-                print(end=" ")
+
+    for i in range(n):
+        if (i == 0):
+            pascal_tri.append([1])
+        else:
+            cur_row = []
             for j in range(i + 1):
+                if (j == 0 or j == i):
+                    cur_row.append(1)
+                else:
+                    cur_row.append(pascal_tri[i - 1][j - 1] +
+                                   pascal_tri[i - 1][j])
+
+            pascal_tri.append(cur_row)
+
+    return (pascal_tri)
