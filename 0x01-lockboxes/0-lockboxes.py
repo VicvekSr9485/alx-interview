60d1ff400a817bf52328a9aec966a4f155cf78fe
VicvekSr <Olamideoso65@gmail.com>
2023-01-08 23:03:07 +0000

    fix pycodestyle errors

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -7,12 +7,13 @@ def canUnlockAll(boxes):
     Return: True or False
     """
     unlocked_boxes = [0]
-    
+
     for box_id, box in enumerate(boxes):
         if not box:
             continue
         for key in box:
-            if key < len(boxes) and key not in unlocked_boxes and key != box_id:
+            if key < len(boxes) and key not in \
+                    unlocked_boxes and key != box_id:
                 unlocked_boxes.append(key)
     if len(unlocked_boxes) == len(boxes):
         return True
