ce50a26505b8f9bdcc9eb05b10616908998c83fb
VicvekSr <Olamideoso65@gmail.com>
2023-01-15 13:13:23 +0000

    fixes

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -6,15 +6,11 @@
 def minOperations(n: int) -> int:
     """ Returns the minimum number of operations
     """
-    counter = 0
-
-    if n <= 1:
-        return counter
-
-    for i in range(2, n + 1):
-        while (0 == n % i):
-            counter = counter + i
-            n = n / i
-            if n < i:
-                break
-    return counter
+    a = 0
+    b = 2
+    while n > 1:
+        while n % b == 0:
+            a += b
+            n = n / b
+        b += 1
+    return a
