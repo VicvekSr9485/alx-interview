7548f2a93b5585b860e8f52a958b65fb57b72d52
VicvekSr <Olamideoso65@gmail.com>
2023-01-15 13:10:27 +0000

    fixes

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -6,15 +6,12 @@
 def minOperations(n: int) -> int:
     """ Returns the minimum number of operations
     """
-    counter = 0
-
-    if n <= 1:
-        return counter
-
+    if n <= 0:  # If the desired number of H's is impossible to achieve
+        return 0
+    dp = [0] * (n + 1)  # Create an array to store the minimum number of operations needed for each value of n
     for i in range(2, n + 1):
-        while (0 == n % i):
-            counter = counter + i
-            n = n / i
-            if n < i:
-                break
-    return counter
+        dp[i] = i  # Set the initial value of dp[i] to i
+        for j in range(2, i):
+            if i % j == 0:  # If i is divisible by j
+                dp[i] = min(dp[i], dp[j] + 1 + i // j)  # Update dp[i] with the minimum number of operations needed
+    return dp[n]  # Return the minimum number of operations needed for n H's
