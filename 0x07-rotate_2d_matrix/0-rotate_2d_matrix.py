31b05f2ab9c7750462c1133e33f0b80e8b17ab13
VicvekSr <Olamideoso65@gmail.com>
2023-02-14 05:04:42 +0000

    add 2D matrix in-place rotation

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -10,15 +10,15 @@ def rotate_2d_matrix(matrix):
     while left < right:
         for i in range(right - left):
             top, bottom = left, right
-            
+
             # save the top left element
-            topleft =matrix[top][left + i]
-            
+            topleft = matrix[top][left + i]
+
             # move bottom left into top left
             matrix[top][left + i] = matrix[bottom - i][left]
 
             # move bottom right into bottom left
-            matrix[bottom -i][left] = matrix[bottom][right - i]
+            matrix[bottom - i][left] = matrix[bottom][right - i]
 
             # move top right into bottom right
             matrix[bottom][right - i] = matrix[top + i][right]
