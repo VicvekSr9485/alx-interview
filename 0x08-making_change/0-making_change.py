c80b772ba6765853f2513f28ac1f9008cc442654
VicvekSr <Olamideoso65@gmail.com>
2023-02-23 20:36:52 +0000

    add a readme file

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" Solution to Make Change Interview Problem """
+
+
+def makeChange(coins, total):
+    """ returns fewest number of coins needed to
+        meet a given amount total
+    """
+    
