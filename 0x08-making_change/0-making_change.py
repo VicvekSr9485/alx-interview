f55e03297857ab9230c32a13ed987f6ce4e16359
VicvekSr <Olamideoso65@gmail.com>
2023-02-23 21:52:48 +0000

    fix

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -20,7 +20,5 @@ def makeChange(coins, total):
     for amount in range(1, total + 1):
         for coin in coins:
             if amount - coin >= 0:
-                dp[amount] = min(dp[amount], dp[amount - coin] + 1)
-    if dp[total] < total + 1:
-        return dp[total]
-    return -1
+                dp[amount] = min(dp[amount], 1 + dp[amount - coin])
+    return dp[total] if dp[total] != total + 1 else -1
