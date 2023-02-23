05bd600c7288745d71a7ffa17ef354618c031b14
VicvekSr <Olamideoso65@gmail.com>
2023-02-23 21:18:34 +0000

    add change comes from within

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -6,4 +6,21 @@ def makeChange(coins, total):
     """ returns fewest number of coins needed to
         meet a given amount total
     """
-    
+    # if total is 0 or less, 0 is returned
+    if total <= 0:
+        return 0
+
+    # next, initialize list(or array) with maximum amount
+    # beyond total amount to calculate a new minimum
+    # Now using dynamic programming:
+    dp = [total + 1] * (total + 1)
+    # setting minimum number of coins for amount 0
+    dp[0] = 0
+
+    for amount in range(1, total + 1):
+        for coin in coins:
+            if amount - coin >= 0:
+                dp[amount] = min(dp[amount], dp[amount - coin] + 1)
+    if dp[total] != total + 1:
+        return dp[total]
+    return -1
