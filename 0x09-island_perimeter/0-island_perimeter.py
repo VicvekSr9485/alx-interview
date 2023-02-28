1ef907840b45c627be935ecd167b4d39006c652c
VicvekSr <Olamideoso65@gmail.com>
2023-02-28 06:26:26 +0000

    add island perimeter solution

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -5,23 +5,24 @@
 def island_perimeter(grid):
     """ returns the perimeter of the island described in grid
     """
-    rows = len(grid)
-    cols = len(grid[0])
+    visited = set()
 
-    perimeter = 0
-    for i in range(rows):
-        for j in range(cols):
-            if grid[i][j] == 1:
-                # check top neighbor
-                if i == 0 or grid[i - 1][j] == 0:
-                    perimeter += 1
-                # check bottom neighbor
-                if i == rows - 1 and grid[i + 1][j] == 0:
-                    perimeter += 1
-                # check left neighbor
-                if j == 0 or grid[i][j + 1] == 0:
-                    perimeter += 1
-                # check right neighbor
-                if j == cols - 1 and grid[i][j + 1] == 0:
-                    perimeter += 1
-    return perimeter
+    def dfs(i, j):
+        """ depth-first search """
+        if i >= len(grid) or j >= len(grid[0]) or \
+                i < 0 or j < 0 or grid[i][j] == 0:
+            return 1
+        if (i, j) in visited:
+            return 0
+
+        visited.add((i, j))
+        perim = dfs(i, j + 1)
+        perim += dfs(i + 1, j)
+        perim += dfs(i, j - 1)
+        perim += dfs(i - 1, j)
+        return perim
+
+    for i in range(len(grid)):
+        for j in range(len(grid[0])):
+            if grid[i][j]:
+                return dfs(i, j)
